45a874f50c52a6827503d026d1a510a4bd9b200c
Christoph Gohlke <cgohlke@uci.edu>
2019-02-20 09:44:01 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,14 +16,18 @@ with open('lfdfiles/lfdfiles.py') as fh:
     code = fh.read()
 
 version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
-description = re.search(r'"""(.*)\.[\r\n?|\n]', code).groups()[0]
-readme = re.search(r'[\r\n?|\n]{2}"""(.*)"""[\r\n?|\n]{2}from', code,
+
+description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+
+readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from', code,
                    re.MULTILINE | re.DOTALL).groups()[0]
-license = re.search(r'(# Copyright.*?[\r\n?|\n])[\r\n?|\n]+""', code,
-                    re.MULTILINE | re.DOTALL).groups()[0]
 
 readme = '\n'.join([description, '=' * len(description)]
                    + readme.splitlines()[1:])
+
+license = re.search(r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""', code,
+                    re.MULTILINE | re.DOTALL).groups()[0]
+
 license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
@@ -64,12 +68,12 @@ setup_args = dict(
     zip_safe=False,
     platforms=['any'],
     classifiers=[
-        'Development Status :: 7 - Inactive',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Programming Language :: C',
+        'Programming Language :: Cython',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
