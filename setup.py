933b5e43db0e11e8808452b5ed6636659772b650
Christoph Gohlke <cgohlke@uci.edu>
2020-01-09 16:31:26 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # lfdfiles/setup.py
 
 """Lfdfiles package setuptools script."""
@@ -18,8 +17,8 @@ version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
 
 description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
 
-readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from', code,
-                   re.MULTILINE | re.DOTALL).groups()[0]
+readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+                   code, re.MULTILINE | re.DOTALL).groups()[0]
 
 readme = '\n'.join([description, '=' * len(description)] +
                    readme.splitlines()[1:])
@@ -31,6 +30,7 @@ license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
     with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
     with open('README.rst', 'w') as fh:
         fh.write(readme)
@@ -71,17 +71,24 @@ setup_args = dict(
     author='Christoph Gohlke',
     author_email='cgohlke@uci.edu',
     url='https://www.lfd.uci.edu/~gohlke/',
-    python_requires='>=2.7',
+    python_requires='>=3.6',
     install_requires=[
-        'numpy>=1.11.3',
+        'numpy>=1.14.5',
         'tifffile>=2019.7.2',
         'click',
     ],
     setup_requires=[
         'setuptools>=18.0',
-        'numpy>=1.11.3',
+        'numpy>=1.14.5',
     ],
-    extras_require={'all': ['matplotlib>=2.2', ]},
+    extras_require={
+        'all': [
+            'matplotlib>=3.1',
+            'czifile>=2019.7.2',
+            'oiffile>=2020.1.1',
+            'netpbmfile>=2020.1.1',
+        ]
+    },
     tests_require=['pytest'],
     packages=['lfdfiles'],
     entry_points={'console_scripts': ['lfdfiles=lfdfiles.__main__:main']},
@@ -95,12 +102,10 @@ setup_args = dict(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Cython',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
 
