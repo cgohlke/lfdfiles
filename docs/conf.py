d5b94260ef8cf2dcfda72100a36a0d811c5624f9
Christoph Gohlke <cgohlke@uci.edu>
2022-06-06 22:40:21 -0700

    Add docs/conf.py

---
+++ docs/conf.py
@@ -0,0 +1,47 @@
+# lfdfiles/docs/conf.py
+
+import os
+import sys
+
+here = os.path.dirname(__file__)
+sys.path.insert(0, os.path.split(here)[0])
+
+import lfdfiles
+
+project = 'lfdfiles'
+copyright = '2012-2022, Christoph Gohlke'
+author = 'Christoph Gohlke'
+version = lfdfiles.__version__
+
+extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    # 'sphinxcontrib.spelling',
+    # 'sphinx.ext.viewcode',
+    # 'sphinx.ext.autosectionlabel',
+    # 'numpydoc',
+    # 'sphinx_issues',
+]
+
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+html_static_path = ['_static']
+html_css_files = ['custom.css']
+html_show_sourcelink = False
+
+autodoc_member_order = 'bysource'  # bysource, groupwise
+autodoc_default_flags = ['members']
+autodoc_typehints = 'description'
+autodoc_type_aliases = {'ArrayLike': 'numpy.ArrayLike'}
+autoclass_content = 'class'
+autosectionlabel_prefix_document = True
+autosummary_generate = True
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
